869afd7b1a159e17260a94bb098e73cfc78c4ae8
Dominic Griesel <dominic.griesel@nabucasa.com>
2022-08-24 14:39:03 +0200

    fix: typo in test script and cleanup readme

--- .github/test-scripts/test.cjs
+++ .github/test-scripts/test.cjs
@@ -2,5 +2,5 @@ const pQueue = require("@esm2cjs/p-queue").default;
 const { AbortError } = require("@esm2cjs/p-queue");
 const assert = require("assert");
 
-assert(typeof pTimeout === "function");
+assert(typeof pQueue === "function");
 assert(typeof AbortError === "function");
